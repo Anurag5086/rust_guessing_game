8d573ab3ae78a543b0debcf39e744ef607d74907
Anurag5086 <anuraggupta5086@gmail.com>
2021-11-24 22:51:00 +0400

    Guess done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn guessgame() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The Random Number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => println!("Yup! You found it!"),
+            Ordering::Greater => println!("Your number is Greater."),
+            Ordering::Less => println!("Your number is Smaller."),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    guessgame();
 }
